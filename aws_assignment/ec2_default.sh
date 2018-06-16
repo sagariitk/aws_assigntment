0bf63d058ad61fe4da6f53a975301dc862b0f215
Sagar Yadav <sagar.yadav@quantiphi.com>
2018-06-16 18:00:02 +0530

    extra file deleted

--- aws_assignment/ec2_default.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/sh
-
-aws events put-rule --name "sagar_ec2_start" --schedule-expression "cron(0 10 *$
-aws events put-rule --name "sagar_ec2_stop" --schedule-expression "cron(0 20 * $
-
-
-
